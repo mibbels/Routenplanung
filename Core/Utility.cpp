39c6b6933363ab4684042c25647462a5f2e76522
Zanget3u <tim.krueger1805@googlemail.com>
2022-10-05 14:26:24 +0200

    Delta encoding of uint32_t works for the 5 test cases

--- Core/Utility.cpp
+++ Core/Utility.cpp
@@ -5,6 +5,24 @@ namespace Core
     #define PBSTR   "############################################################"
     #define PBWIDTH 60
 
+    bool Utility::Bit_Is_Set(uint32_t value, uint32_t index)
+    {
+        return (value & (1 << index)) != 0;
+    }
+
+    bool Utility::Array_Has_Value(const uint8_t* arr, uint8_t length, uint8_t index)
+    {
+        for(uint8_t i = index; i < length; i++)
+        {
+            if(arr[i] != 0)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     void Utility::Display_ui8vec(const ui8vec& vector, const uint8_t numberOfBytes)
     {
         //Sanity check
@@ -24,33 +42,63 @@ namespace Core
                node.nodeCount, node.dataLength, node.id, node.version, node.lat, node.lon);
     }
 
-    std::vector<uint8_t> Utility::DeltaEncode_sInt32(int32_t integer)
+    std::vector<uint8_t> Utility::DeltaEncode_uInt32(uint32_t value)
     {
-        uint8_t byte0 = integer >> 7;
-        uint8_t byte1 = integer & 0x00ff;
+        //msb := most significant bit
+        //MSB := MOST SIGNIFICANT BYTE
 
+        //Storage container
         std::vector<uint8_t> res;
 
-        //Disregard bit 7 (most significant bit) of every byte
-        byte0 &= 0x7f;
-        byte1 &= 0x7f;
+        //Get bytes out of the given value
+        uint8_t byte0 = value & 0x7f;
+        uint8_t byte1 = value >> 7;
+        uint8_t byte2 = value >> 15;
+        uint8_t byte3 = value >> 23;
+        uint8_t paddingByte = 0x00;
 
-        //Set indicator bit (next byte belongs to the same number)
-        if(byte0 != 0x00)
+        //Create byte array
+        uint8_t byteArray[5] = {byte0, byte1, byte2, byte3, paddingByte};
+
+        //Iterate over all bytes except the last one
+        for(uint8_t i = 0; i < 4; i++)
         {
-            byte1 |= 0x80;
-            res.push_back(byte1);
-            res.push_back(byte0);
-
-            return res;
+            //Check if msb is set
+            if(Bit_Is_Set(byteArray[i], 7))
+            {
+                //Add a +1 to the next byte and reset msb
+                byteArray[i + 1] |= 0x01;
+                byteArray[i] &= 0x7f;
+            }
         }
 
-        res.push_back(byte1);
+        //Iterate over all bytes except the last one
+        for(uint8_t i = 0; i < 4; i++)
+        {
+            //Check if there is a following byte which is not zero
+            if(Array_Has_Value(byteArray, 5, i+1))
+            {
+                //Set indicator bit (next byte belongs to the same number)
+                byteArray[i] |= 0x80;
+
+                //Save byte in vector
+                res.push_back(byteArray[i]);
+            }
+
+            //Else we got the last byte
+            else
+            {
+                //Don't flip any bit, just save the byte (MSB)
+                res.push_back(byteArray[i]);
+
+                break;
+            }
+        }
 
         return res;
     }
 
-    int32_t Utility::DeltaDecode_sInt32(uint8_t* rawData, uint8_t dataLength)
+    /*int32_t Utility::DeltaDecode_sInt32(uint8_t* rawData, uint8_t dataLength)
     {
         if(dataLength == 1)
         {
@@ -69,7 +117,7 @@ namespace Core
 
             return res;
         }
-    }
+    }*/
 
     void Utility::PrintProgressBar(double percentage)
     {
--- Core/Utility.hpp
+++ Core/Utility.hpp
@@ -7,12 +7,16 @@ namespace Core
 {
     class Utility
     {
+        private:
+            static bool    Bit_Is_Set(uint32_t value, uint32_t index);
+            static bool    Array_Has_Value(const uint8_t* arr, uint8_t length, uint8_t index);
+
         public:
             static void    Display_ui8vec(const ui8vec& vector, uint8_t numberOfBytes);
             static void    Display_Node(const Node& node);
 
-            static std::vector<uint8_t> DeltaEncode_sInt32(int32_t integer);
-            static int32_t              DeltaDecode_sInt32(uint8_t* rawData, uint8_t dataLength);
+            static std::vector<uint8_t> DeltaEncode_uInt32(uint32_t value);
+            //static int32_t              DeltaDecode_sInt32(uint8_t* rawData, uint8_t dataLength);
 
             static void    PrintProgressBar(double percentage);
     };
